afc03a48ccebc2d7608fbf46a7112a5b1520b76a
Unknown <Zhukjeka1998@yandex.by>
2017-12-04 21:30:03 +0300

    name of numbers

---
+++ Utilities.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+
+const int PIC_SIZE = 30;
+const int HEIGHT = 720;
+const int WIDTH = 720;
+const int MARGIN_TOP = 300;
+const int WIDTH_RESET = 720 / 2.4;
+const int WIDTH_POWER = 720 / 2.4 + 70;
+const int SCORE_UP = 10;
